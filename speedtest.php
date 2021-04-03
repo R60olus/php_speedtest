4a4aa020644e532d323b8144cf4ea40b6b6b228e
R60olus <62094884+R60olus@users.noreply.github.com>
2021-04-03 15:10:14 +0200

    Update speedtest.php

--- speedtest.php
+++ speedtest.php
@@ -17,7 +17,7 @@ while(true) {
 	// echo "1";		//only 1 as data (high compression)
         //echo mt_rand();	//random Data (high CPU usage)
         //create Random file (~1GB) with: openssl rand -out file.bin -base64 $(( 2**30 * 3/4 ))
-        readfile("file.bin")
+        readfile("file.bin");
 }
 
 ?>
